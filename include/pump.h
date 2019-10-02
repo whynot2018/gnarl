fe011fb72f940050a2e3765a84c4ef9cf61062e1
whynot2018 <48062504+whynot2018@users.noreply.github.com>
2019-10-02 09:15:15 +0200

    Create pump.h

---
+++ include/pump.h
@@ -0,0 +1,3 @@
+#define PUMP_ID         "ABCDEF"        // pump serial number (note that this is a string constant)
+#define PUMP_FREQUENCY  868550000       // pump frequency
+#define MMTUNE_START    868400000       // starting frequency for mmtune scans
