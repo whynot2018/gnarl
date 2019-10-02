f6c4217360bf333399e4a5467f6b91a5fdd8133c
whynot2018 <48062504+whynot2018@users.noreply.github.com>
2019-10-02 09:19:05 +0200

    Create nightscout_config.h

---
+++ include/nightscout_config.h
@@ -0,0 +1,5 @@
+// DNS hostname, not a URL
+#define NIGHTSCOUT_HOST         "XXXXXXX.herokuapp.com"
+
+// 40-character SHA-1 hash of your Nightscout API secret
+#define NIGHTSCOUT_API_SECRET   "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
